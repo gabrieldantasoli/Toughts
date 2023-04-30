d84ffe39f2c8f05029b8d6acb596e9abc9abc4f6
gabriel dantas de oliveira <gabriel36dantas@gmail.com>
2023-04-30 19:21:56 -0300

    Finishing basic Project Structure - TOUGHTS

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ const app = express();
 
 const conn = require('./db/conn');
 
+const hbs = exphbs.create({
+    partialsDir: ['views/partials']
+});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+// Receber resposta do body
 app.use(
     express.urlencoded({
         extended: true
@@ -16,13 +24,41 @@ app.use(
 
 app.use(express.json());
 
-const hbs = exphbs.create({
-    partialsDir: ['views/partials']
+// Session middleware
+app.use(
+    session({
+        name: "session",
+        secret: "our_secret",
+        resave: false,
+        saveUninitialized: false,
+        store: new FileStore({
+            logFn: function() {},
+            path: require('path').join(require('os').tmpdir(), 'sessions'),
+        }),
+        cookie: {
+            secure: false,
+            maxAge: 360000,
+            expires: new Date(Date.now() + 360000),
+            httpOnly: true,
+        }
+    }),
+);
+
+// flash messages
+app.use(flash());
+
+// public path
+app.use(express.static('public'));
+
+// set session to res
+app.use((req, res, next) => {
+    if (req.session.userid) {
+        res.locals.session = req.session;
+    }
+
+    next();
 });
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-app.use(express.static('public'));
 
 conn
     .sync()

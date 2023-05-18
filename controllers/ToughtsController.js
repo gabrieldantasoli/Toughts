0d03fae9bb9775cfde45ab7b5901e94b57c08914
gabriel dantas de oliveira <gabriel36dantas@gmail.com>
2023-05-17 21:49:32 -0300

    dashboard

--- controllers/ToughtsController.js
+++ controllers/ToughtsController.js
@@ -7,7 +7,25 @@ module.exports = class ToughtController {
     }
 
     static async dashboard(req, res) {
-        res.render('toughts/dashboard');
+        const userId1 = req.session.userid;
+        const user = await User.findOne(
+            { 
+                where: {id: userId1},
+                include: Tought,
+                plain: true,
+            }
+        );
+
+        // Check if user exists
+        if (!user) {
+            res.redirect('/login');
+        }
+
+        const toughts = user.Toughts.map((result) => result.dataValues);
+
+        console.log(toughts);
+
+        res.render('toughts/dashboard', { toughts });
     }
 
     static createTought(req, res) {
@@ -17,8 +35,9 @@ module.exports = class ToughtController {
     static async createToughtSave(req, res) {
         const tought = {
             title: req.body.title,
-            UserId: req.session.Userid
+            UserId: req.session.userid
         }
+
 
         await Tought.create(tought);
 
@@ -31,6 +50,5 @@ module.exports = class ToughtController {
         } catch (err) {
             console.log(err);
         }
-        
     }
 }

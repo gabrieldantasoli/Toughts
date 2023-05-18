29db470e9250cb306859f5f729859f670f382d6d
gabriel dantas de oliveira <gabriel36dantas@gmail.com>
2023-05-18 19:07:24 -0300

    Updating Toughts

--- routes/toughtsRoutes.js
+++ routes/toughtsRoutes.js
@@ -7,6 +7,7 @@ const checkAuth1 = require('../helpers/auth').checkAuth;
 
 router.get('/add', checkAuth1, ToughtController.createTought);
 router.post('/add', checkAuth1, ToughtController.createToughtSave);
+router.get('/edit/:id', checkAuth1, ToughtController.updateTought);
 router.get('/dashboard', checkAuth1, ToughtController.dashboard);
 router.post('/remove', checkAuth1, ToughtController.removeToughtSave);
 router.get('/', ToughtController.showToughts);
